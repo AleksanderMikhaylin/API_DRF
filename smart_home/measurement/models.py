be1b98de15678d21ae3f76379e33a3f27c20ff04
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-26 16:38:54 +1100

    Update models.py

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 
-
 class Sensor(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
@@ -11,10 +10,9 @@ class Sensor(models.Model):
 
 
 class Measurement(models.Model):
-    sensor_id = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     temperature = models.FloatField()
     created_at = models.DateTimeField()
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensors')
 
     def __str__(self):
         return 'Показания'
-
