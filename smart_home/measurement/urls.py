2c522ea3c4e02b33d92e9308a8ca80b4eeeccf40
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-27 13:48:38 +1100

    Update urls.py

--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -6,5 +6,5 @@ from .views import SensorListView, SensorView
 urlpatterns = [
     path('list_sensor/', SensorListView.as_view()),
     path('add_sensor/', SensorAdd.as_view()),
-    path('sensor/<pk>/', SensorView.as_view()),
+    path('sensor/<int:pk>/', SensorView.as_view()),
 ]
