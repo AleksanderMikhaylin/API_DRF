f13c0b8259025c28bedd35679060e253ba3d64b8
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-27 11:08:21 +1100

    Update serializers.py

--- smart_home/measurement/serializers.py
+++ smart_home/measurement/serializers.py
@@ -5,14 +5,35 @@ from .models import Sensor, Measurement
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ['sensor_id', 'temperature', 'created_at']
+        fields = ['temperature', 'created_at', 'sensor_id']
+
+
+class CreateMeasurementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'created_at', 'sensor_id']
+
+    def create(self, validated_data):
+
+        measurement = Measurement.objects.create(
+            sensor_id=self.initial_data.get('sensor_id'),
+            temperature=self.initial_data.get('temperature'),
+            created_at=self.initial_data.get('created_at'),
+        )
+        # measurement = Measurement.objects.create(
+        #     sensor_id=self.initial_data.get('sensor_id'),
+        #     temperature=validated_data.get('temperature'),
+        #     created_at=validated_data.get('created_at'),
+        # )
+        return measurement
 
 
 class SensorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Sensor
-        fields = ['name', 'description']
+        fields = ['id', 'name', 'description']
 
 
 class SensorDetailSerializer(serializers.ModelSerializer):
