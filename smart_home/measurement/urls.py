e863a2e12870a4ae28847773e257776f983222c2
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-27 16:38:04 +1100

    Update urls.py

--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SensorListView, SensorView
+from .views import SensorListView, SensorView, SensorAdd
 
 
 urlpatterns = [
+    path('sensor/', SensorAdd.as_view()),
     path('sensors/', SensorListView.as_view()),
     path('sensors/<int:pk>/', SensorView.as_view()),
 ]
