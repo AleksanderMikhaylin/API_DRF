19e6556c1c7ddd93a020536b56973c92e0c11740
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-28 17:30:36 +1100

    Update views.py

--- smart_home/measurement/views.py
+++ smart_home/measurement/views.py
@@ -1,91 +1,60 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from measurement.serializers import SensorSerializer, SensorDetailSerializer, MeasurementSerializer, CreateMeasurementSerializer
-from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorSerializer, SensorDetailSerializer, CreateMeasurementSerializer
+from measurement.models import Sensor
 from datetime import datetime
-
-
-class SensorListView(ListAPIView):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
 
 
 class SensorView(APIView):
 
-    def get(self, request, pk):
+    def get(self, request, pk=None):
 
-        sensors = Sensor.objects.filter(id=int(pk))
-        serializer = SensorDetailSerializer(sensors, many=True)
+        if pk is None:
+            sensors = Sensor.objects.all()
+            serializer = SensorSerializer(sensors, many=True)
+        else:
+            sensors = Sensor.objects.filter(id=int(pk))
+            serializer = SensorDetailSerializer(sensors, many=True)
+
         return Response({"sensor": serializer.data})
 
-    def post(self, request, pk):
+    def post(self, request, pk=None):
 
-        sensor = Sensor.objects.filter(id=int(pk)).first()
-        if sensor is None:
-            return Response({f'err': f'Датчик с id={pk} не существует!'})
+        if pk is None:
+            name = request.data.get("name")
+            description = request.data.get("description")
+            if name is None:
+                return Response({'err': 'Не верное название датчика'})
+            serializer = SensorSerializer(data=dict(name=name, description=description))
+            if serializer.is_valid(raise_exception=True):
+                sensor_saved = serializer.save()
+                return Response({"success": f"Sensor '{sensor_saved.name}' создан успешно, id={sensor_saved.id}"})
+            return Response({'err': 'Ошибка создания датчика'})
+        else:
+            sensor = Sensor.objects.filter(id=int(pk)).first()
+            if sensor is None:
+                return Response({f'err': f'Датчик с id={pk} не существует!'})
 
-        temperature = request.data.get("temperature")
-        if temperature is None:
-            return Response({'err': 'Не найден параметр температуры'})
-        try:
-            temperature_float = float(temperature)
-        except:
-            return Response({'err': 'Не верный параметр температуры'})
+            temperature = request.data.get("temperature")
+            if temperature is None:
+                return Response({'err': 'Не найден параметр температуры'})
+            try:
+                temperature_float = float(temperature)
+            except:
+                return Response({'err': 'Не верный параметр температуры'})
 
-        data = {
-            'sensor_id': int(pk),
-            'temperature': '{:.1f}'.format(temperature_float),
-            'created_at': datetime.now(),
-        }
+            data = {
+                'sensor_id': int(pk),
+                'temperature': '{:.1f}'.format(temperature_float),
+                'created_at': datetime.now(),
+            }
 
-        # serializer = MeasurementSerializer(data=data)
-        serializer = CreateMeasurementSerializer(data=data)
+            serializer = CreateMeasurementSerializer(data=data)
 
-        if serializer.is_valid(raise_exception=True):
-            sensor_saved = serializer.save()
-            return Response({"success": f"Показания температуры {sensor_saved.temperature}C для датчика '{sensor.name}' добавлены успешно"})
-        return Response({'err': f'Ошибка добавления показания для датчика {sensor.name}'})
-
-    # def put(self, request, pk):
-    # 
-    #     sensor = Sensor.objects.filter(id=int(pk)).first()
-    #     if sensor is None:
-    #         return Response({f'err': f'Датчик с id={pk} не существует!'})
-    # 
-    #     name = request.data.get("name")
-    #     description = request.data.get("description")
-    #     if name is None and description is None:
-    #         return Response({'err': 'Переданы не верные парамтеры'})
-    # 
-    #     data = {
-    #         'name': sensor.name,
-    #         'description': sensor.description,
-    #     }
-    # 
-    #     serializer = Serializer(data=data)
-    # 
-    #     if serializer.is_valid(raise_exception=True):
-    #         sensor_saved = serializer.save()
-    #         return Response({"success": f"Показания температуры {sensor_saved.temperature}C для датчика '{sensor.name}' добавлены успешно"})
-    #     return Response({'err': f'Ошибка добавления показания для датчика {sensor.name}'})
-
-
-class SensorAdd(APIView):
-
-    def get(self, request):
-        return Response({"err": "Method 'GET' not allowed."})
-    def post(self, request):
-        # print(request)
-        name = request.data.get("name")
-        description = request.data.get("description")
-        if name is None:
-            return Response({'err': 'Не верное название датчика'})
-        serializer = SensorSerializer(data=dict(name=name, description=description))
-        if serializer.is_valid(raise_exception=True):
-            sensor_saved = serializer.save()
-            return Response({"success": f"Sensor '{sensor_saved.name}' создан успешно, id={sensor_saved.id}"})
-        return Response({'err': 'Ошибка создания датчика'})
+            if serializer.is_valid(raise_exception=True):
+                sensor_saved = serializer.save()
+                return Response({"success": f"Показания температуры {sensor_saved.temperature}C для датчика '{sensor.name}' добавлены успешно"})
+            return Response({'err': f'Ошибка добавления показания для датчика {sensor.name}'})
